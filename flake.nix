d31207c40c441b96ebefbeefea07bf3f3c1a4da6
quantinium03 <quant@quantinium.dev>
2025-07-22 22:22:06 +0530

    fixed flake

--- flake.nix
+++ flake.nix
@@ -7,85 +7,86 @@
   };
 
   outputs = { self, nixpkgs, flake-utils, ... }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = import nixpkgs { inherit system; };
-        coderunner = pkgs.rustPlatform.buildRustPackage rec {
-          pname = "coderunner";
-          version = "0.1.0";
-          src = pkgs.fetchFromGitHub {
-            owner = "quantinium3";
-            repo = "coderunner";
-            rev = "61153a03bcfba107541b1014329eeb0f184b567e";
-            sha256 = "sha256-iv8kUHUywUrlbucIRqoen1PcSNSK9G86crPMzqRdN9U=";
+    flake-utils.lib.eachDefaultSystem
+      (system:
+        let
+          pkgs = import nixpkgs { inherit system; };
+          coderunner = pkgs.rustPlatform.buildRustPackage rec {
+            pname = "coderunner";
+            version = "0.1.0";
+            src = pkgs.fetchFromGitHub {
+              owner = "quantinium3";
+              repo = "coderunner";
+              rev = "61153a03bcfba107541b1014329eeb0f184b567e";
+              sha256 = "sha256-iv8kUHUywUrlbucIRqoen1PcSNSK9G86crPMzqRdN9U=";
+            };
+            doCheck = false;
+            cargoHash = "sha256-cJ1h6RrUhjT0sGgEa0B6OP1luTdFWaz+8LtVyVeDwSs=";
+            nativeBuildInputs = with pkgs; [
+              pkg-config
+            ];
+            buildInputs = with pkgs; [
+              openssl
+              zig
+              crystal
+              dmd
+              dart
+              go
+              groovy
+              ghc
+              julia
+              nix
+              odin
+              perl
+              ruby
+              rustc
+              scala
+              bfc
+              R
+              clang
+              bun
+              python3
+              go
+              luaPackages.lua
+              nix
+              perl
+              R
+              ruby
+              rustc
+              scala
+            ];
           };
-          doCheck = false;
-          cargoHash = "sha256-cJ1h6RrUhjT0sGgEa0B6OP1luTdFWaz+8LtVyVeDwSs=";
-          nativeBuildInputs = with pkgs; [
-            pkg-config
-            zig
-            crystal
-            dmd
-            dart
-            go
-            groovy
-            ghc
-            julia
-            nix
-            odin
-            perl
-            ruby
-            rustc
-            scala
-            bfc
-            R
-            clang
-            bun
-            python3
-            go
-            luaPackages.lua
-            nix
-            perl
-            R
-            ruby
-            rustc
-            scala
-          ];
-          buildInputs = with pkgs; [
-            openssl
-            zig
-            crystal
-            dmd
-            dart
-            go
-            groovy
-            ghc
-            julia
-            nix
-            odin
-            perl
-            ruby
-            rustc
-            scala
-            bfc
-            R
-            clang
-            bun
-            python3
-            go
-            luaPackages.lua
-            nix
-            perl
-            R
-            ruby
-            rustc
-            scala
-          ];
+        in
+        {
+          packages.default = coderunner;
+          packages.coderunner = coderunner;
+        }
+      ) // {
+      nixosModules.coderunner = { config, pkgs, lib, ... }: {
+        options.services.coderunner = {
+          enable = lib.mkEnableOption "coderunner backend";
         };
-      in
-      {
-        packages.default = coderunner;
-        packages.coderunner = coderunner;
-      }
-    );
+        config = lib.mkIf config.services.coderunner.enable {
+          systemd.services.coderunner = {
+            description = "coderunner backend";
+            after = [ "network.target" ];
+            wantedBy = [ "multi-user.target" ];
+            serviceConfig = {
+              ExecStart = "${self.packages.${pkgs.system}.coderunner}/bin/comphub";
+              Restart = "always";
+              RestartSec = "10s";
+              StandardOutput = "journal";
+              StandardError = "journal";
+              User = "coderunner";
+              Group = "coderunner";
+              DynamicUser = true;
+              NoNewPrivileges = true;
+              PrivateTmp = true;
+              ProtectSystem = "strict";
+              ProtectHome = true;
+            };
+          };
+        };
+      };
+    };
 }
