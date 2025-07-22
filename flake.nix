96f3cd35ee4c83a9398cafa52a49c3b8fb779c6b
quantinium03 <quant@quantinium.dev>
2025-07-22 20:54:52 +0530

    flake made

--- flake.nix
+++ flake.nix
@@ -1,96 +1,91 @@
 {
-  description = "idk man";
+  description = "coderunner";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
   };
 
   outputs = { self, nixpkgs, flake-utils, ... }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = import nixpkgs { inherit system; };
-        openssl = pkgs.openssl;
+        coderunner = pkgs.rustPlatform.buildRustPackage rec {
+          pname = "coderunner";
+          version = "0.1.0";
+          src = pkgs.fetchFromGitHub {
+            owner = "quantinium3";
+            repo = "coderunner";
+            rev = "61153a03bcfba107541b1014329eeb0f184b567e";
+            sha256 = "sha256-iv8kUHUywUrlbucIRqoen1PcSNSK9G86crPMzqRdN9U=";
+          };
+          doCheck = false;
+          cargoHash = "sha256-cJ1h6RrUhjT0sGgEa0B6OP1luTdFWaz+8LtVyVeDwSs=";
+          nativeBuildInputs = with pkgs; [
+            pkg-config
+            zig
+            crystal
+            dmd
+            dart
+            go
+            groovy
+            ghc
+            julia
+            nix
+            odin
+            perl
+            ruby
+            rustc
+            scala
+            bfc
+            R
+            clang
+            bun
+            python3
+            go
+            luaPackages.lua
+            nix
+            perl
+            R
+            ruby
+            rustc
+            scala
+          ];
+          buildInputs = with pkgs; [
+            openssl
+            zig
+            crystal
+            dmd
+            dart
+            go
+            groovy
+            ghc
+            julia
+            nix
+            odin
+            perl
+            ruby
+            rustc
+            scala
+            bfc
+            R
+            clang
+            bun
+            python3
+            go
+            luaPackages.lua
+            nix
+            perl
+            R
+            ruby
+            rustc
+            scala
+          ];
+        };
       in
       {
-        devShells.default = pkgs.mkShell {
-          packages = [
-            openssl
-            pkgs.clang
-            pkgs.kotlin
-            pkgs.scala_3
-            pkgs.groovy
-            pkgs.dart
-            pkgs.ruby
-            pkgs.lua5
-            pkgs.julia
-            pkgs.R
-            pkgs.perl
-            pkgs.crystal
-            pkgs.nim
-            pkgs.ghc
-            pkgs.elmPackages.elm
-            pkgs.vlang
-            pkgs.coffeescript
-            pkgs.dotnet-sdk
-            pkgs.fsharp
-            pkgs.ocaml
-            pkgs.elixir
-            pkgs.erlang
-            pkgs.php
-            pkgs.gfortran
-            pkgs.gnat
-            pkgs.fpc
-            pkgs.racket
-            pkgs.clojure
-            pkgs.bash
-            pkgs.powershell
-            pkgs.nasm
-            pkgs.gprolog
-            pkgs.fish
-            pkgs.agda
-            pkgs.purescript
-            pkgs.gleam
-            pkgs.odin
-            pkgs.gforth
-            pkgs.gst
-            pkgs.tcl
-            pkgs.jq
-            pkgs.algol68g
-            pkgs.idris2
-            pkgs.factor-lang
-            pkgs.sbcl
-            pkgs.mercury
-            pkgs.janet
-            pkgs.swift
-            pkgs.dmd
-            pkgs.zig
-            pkgs.bacon
-            pkgs.pkg-config
-            pkgs.chicken
-            pkgs.chez
-            pkgs.fennel
-            pkgs.clang
-            pkgs.gnustep-libobjc
-            pkgs.gnustep-base
-          ];
-
-          env = {
-            OPENSSL_DIR = "${openssl.dev}";
-            OPENSSL_LIB_DIR = "${openssl.out}/lib";
-            OPENSSL_INCLUDE_DIR = "${openssl.dev}/include";
-
-            PKG_CONFIG_PATH = "${openssl.out}/lib/pkgconfig";
-
-            LD_LIBRARY_PATH = "${openssl.out}/lib:${pkgs.swiftPackages.Dispatch}/lib";
-          };
-
-          stdenv = pkgs.swift.stdenv;
-
-          shellHook = ''
-            export RUSTFLAGS="-C link-arg=-L${openssl.out}/lib"
-          '';
-        };
+        packages.default = coderunner;
+        packages.coderunner = coderunner;
       }
     );
 }
